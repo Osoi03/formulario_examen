134227ad26118f51795fa732be041a9903db5c1e
Harold Gutierrez <162295916+Osoi03@users.noreply.github.com>
2024-04-16 14:34:47 +0200

    Add files via upload

---
+++ bd.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE TEXT;
+
+USE TEXT;
+
+create table evaluacion(
+    nombre VARCHAR (30),
+    pregunta1 VARCHAR (400),
+    pregunta2 VARCHAR (400),
+    pregunta3 VARCHAR (400),
+    pregunta4 VARCHAR (400),
+    pregunta5 VARCHAR (400)
+    );
---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+function conectDB()
+{
+    $servidor="localhost";
+    $usuario="root";
+    $password="";
+    $base="text";
+
+    $conexion = mysqli_connect($servidor, $usuario, $password, $base)
+or die("No se pudo conectar con la base de datos");
+return $conexion;
+}
+
+
+?>
